bb00a36059e403b6ede4bf5c3a4ab0a6182a839d
Gabriel Colmenares <gbrieldcg@gmail.com>
2021-05-03 20:43:38 -0400

    js file deleted, feature added

--- static/js/instafeed.js
+++
@@ -1,8 +0,0 @@
-var feed = new Instafeed({
-    accessToken: 'IGQVJVX0h0dUdFZA29UUzlTQTJ4b3NhRWtOanBGYnBKM182RThaSUJzaWx3ODJsX0U5cjZAsNGhDNDF3dG9FS04zbW5jdUlCaUdKQ1llT1luLVphQXdMWFJ2a2RES0UyMzhLNThHZAlU5eFVyMlpVTmxuNwZDZD',
-    resolution: 'standard_resolution',
-    target: 'instafeed',
-    limit: 6,
-    template:	'<a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a>'
-  });
-  feed.run();
